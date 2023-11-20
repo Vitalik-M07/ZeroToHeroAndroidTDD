8df381deb4059e71f729ebb79a3db229ef5b5bdb
Vitalik-M07 <vitalik.mahovik@gmail.com>
2023-11-20 16:46:21 +0200

    Поменял текст в тектвьюхе, добавил айдишник текствьюхе и (!) добавил кнопку и поставил ее ниже чем текст.  .

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,29 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        val linearlayout = LinearLayout(this)
+//        linearlayout.id = R.id.rootLayout
+//        val textView = TextView(this)
+//        textView.id = R.id.titleTextView
+//        textView.text = "I am an Android Developer!"
+//
+//        val button = Button(this)
+//        button.id = R.id.changeButton
+//        button.text = "change"
+//
+//
+//        linearlayout.addView(textView)
+//        linearlayout.addView(button)
+
+
+
         setContentView(R.layout.activity_main)
     }
 }
