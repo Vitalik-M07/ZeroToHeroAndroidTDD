75f3f1889f09e69357547e5b48334a03d2f72997
CRESAXIC <cresaxic@gmail.com>
2024-04-03 14:02:13 +0300

    task 9 solved by me

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,43 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rootlayout: ViewGroup
+    private lateinit var textView: TextView
+    private var isTextViewRemoved: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.titleTextView)
+        rootlayout = findViewById(R.id.rootLayout)
+        val button = findViewById<Button>(R.id.removeButton)
+
+        button.setOnClickListener {
+            if (!isTextViewRemoved) {
+                rootlayout.removeView(textView)
+                isTextViewRemoved = true
+            }
+        }
+
+        savedInstanceState?.let {
+            isTextViewRemoved = it.getBoolean("isTextViewRemoved", false)
+            if (isTextViewRemoved) {
+                rootlayout.removeView(textView)
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("isTextViewRemoved", isTextViewRemoved)
+
     }
 }
